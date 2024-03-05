12712f99731d435c0d86a18cd2dab6d95fe397af
Jeff Sember <jpsember@gmail.com>
2024-03-05 12:38:43 -0800

    Issue #1: attempted to get a 'curl' based AWS approach working, but the signing stuff is very confusing

--- src/main/java/remote/AWSHandler.java
+++ src/main/java/remote/AWSHandler.java
@@ -2,11 +2,16 @@ package remote;
 
 import static js.base.Tools.*;
 
+import java.io.File;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 
 import js.base.SystemCall;
 import js.data.DataUtil;
+import js.file.FileException;
+import js.file.Files;
 import js.json.JSList;
 import js.json.JSMap;
 import js.webtools.gen.RemoteEntityInfo;
@@ -55,6 +60,7 @@ public class AWSHandler extends RemoteHandler {
     scMap = null;
     var sc = new SystemCall();
     sc.setVerbose(verbose());
+    sc.alertVerbose();
     mSc = sc;
     return sc();
   }
@@ -238,18 +244,82 @@ public class AWSHandler extends RemoteHandler {
   //    return mEntityMap;
   //  }
 
+  private void webcall(String action) {
+    systemCall();
+    var sc = sc();
+    sc.arg("curl", //
+        "-s", // suppress progress bar
+        "-H", "Content-Type: application/json" //
+    //   "-H", "Authorization: Bearer " + config().accessToken()
+    );
+    sc.arg("-X", action);
+    mSb = new StringBuilder();
+    mParamCount = 0;
+  }
+
+  private String urlEncode(String expr) {
+    return URLEncoder.encode(expr, StandardCharsets.UTF_8);
+  }
+
+  private void param(String key, String value) {
+    var sb = mSb;
+    sb.append(mParamCount == 0 ? '?' : '&');
+    mParamCount++;
+    sb.append(urlEncode(key));
+    sb.append('=');
+    sb.append(urlEncode(value));
+  }
+
   @Override
   public List<KeyPairEntry> keyPairList() {
 
-    
-    if(alert("attempting to use web api instead of aws cli")) {
-    
-    
-    
+    todo("can we use our WebRequest capability, instead of calling curl?");
+    if (alert("attempting to use web api instead of aws cli")) {
+
+      // See https://docs.aws.amazon.com/AWSEC2/latest/APIReference/Query-Requests.html
+
+      // and for 'signature version 4 authorization', see https://how.wtf/aws-sigv4-requests-with-curl.html
+
+      webcall("GET");
+      var sc = new SystemCall();
+      sc.setVerbose(verbose());
+
+      sc.arg("curl");
+
+      // suppress progress bar
+      sc.arg("-s");
+
+      // --user "$AWS_ACCESS_KEY_ID":"$AWS_SECRET_ACCESS_KEY"
+      var c = credentials();
+      var def = c.optJSMap("default");
+      if (def == null)
+        badArg("no 'default' entry in ~/.aws/credentials file");
+      
+       sc.arg("--user",def.get("aws_access_key_id")+":"+def.get("aws_secret_access_key"));
+
+       sc.arg("--aws-sigv4","aws:amz:us-west-2:execute-api");
+      // Is GET implied?
+      sc.arg("-X", "GET");
+
+      var sb = mSb;
+      sb.append("https://ec2.amazonaws.com/");
+    //  param("AWSAccessKeyId",def.get("aws_access_key_id"));
+    //  param("AWSSecretAccessKey",def.get("aws_secret_access_key"));
+      param("Action", "DescribeKeyPairs");
+
+      sc.arg(mSb);
+      if (sc.exitCode() != 0) {
+        alert("got exit code", sc.exitCode(), INDENT, sc.systemErr());
+      }
+      sc.assertSuccess();
+      pr(sc.toString());
+
+      pr(sc.systemErr());
+      pr(sc.systemOut());
+      halt();
+      return arrayList();
     }
-    
-    
-    
+
     // https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-key-pairs.html
     ec2();
     arg("describe-key-pairs");
@@ -280,9 +350,70 @@ public class AWSHandler extends RemoteHandler {
     pr(scOut());
   }
 
+  private JSMap credentials() {
+    if (mCred == null)
+      mCred = parseAWSFile(new File(Files.homeDirectory(), ".aws/credentials"));
+    return mCred;
+  }
+
+  private JSMap mCred;
+
+  private JSMap parseAWSFile(File f) {
+    Files.assertExists(f, "aws credentials file");
+    String s = Files.readString(f);
+    List<String> rows = split(s, '\n');
+
+    // Remove any comment lines; we will assume that this is lines that have '#' in them, and
+    // will generate a warning if there are any such lines that don't start with '#'
+    {
+      List<String> filtered = arrayList();
+      for (String row : rows) {
+        row = row.trim();
+        if (nullOrEmpty(row))
+          continue;
+        if (row.contains("#")) {
+          if (!row.startsWith("#")) {
+            alert("Configuration file:", f, "has unexpected '#' character(s)");
+          }
+          continue;
+        }
+        filtered.add(row);
+      }
+      rows = filtered;
+    }
+
+    JSMap result = map();
+
+    try {
+      JSMap currentGroup = null;
+
+      String groupId = null;
+      for (String row : rows) {
+        if (row.startsWith("[")) {
+          checkArgument(row.endsWith("]"));
+          groupId = row.substring(1, row.length() - 1);
+          checkArgument(!result.containsKey(groupId));
+          currentGroup = new JSMap();
+          result.put(groupId, currentGroup);
+          continue;
+        }
+        List<String> words = split(row, '=');
+        checkArgument(words.size() == 2);
+        String key = words.get(0).trim();
+        String value = words.get(1).trim();
+        currentGroup.put(key, value);
+      }
+    } catch (Throwable t) {
+      throw FileException.withCause(t, "Problem parsing configuration file:", f);
+    }
+    return result;
+  }
+
   private Map<String, RemoteEntry> mEntityMap;
   //  private JSList mErrors;
   //  private JSMap mSysCallOutput;
   private SystemCall mSc;
   private JSMap scMap;
+  private StringBuilder mSb;
+  private int mParamCount;
 }
