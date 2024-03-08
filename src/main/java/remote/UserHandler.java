e426e12e55c3c689ac0c253cd71c921dbc571634
Jeff Sember <jpsember@gmail.com>
2024-03-08 15:28:26 -0800

    Issue #2

--- src/main/java/remote/UserHandler.java
+++ src/main/java/remote/UserHandler.java
@@ -18,20 +18,23 @@ public class UserHandler extends RemoteHandler {
   public void entityCreate(String label, String imageLabel) {
     checkArgument(nullOrEmpty(imageLabel), "images not supported");
     var ent = getUserEntity(label, false);
-    
+
     // We allow create to be called repeatedly on an existing one to modify its parameters
     //    if (ent != null)
     //      badState("entity already exists:", label, INDENT, ent);
-    if (ent == null)
-      ent = RemoteEntityInfo.DEFAULT_INSTANCE;
+    if (ent == null) {
+      ent = RemoteEntityInfo.DEFAULT_INSTANCE.toBuilder().label(label).host(name()).port(22);
+    }
     var rec = ent.toBuilder();
 
-    rec.label(label);
-    rec.host(name());
-    rec.port(22);
+    //    rec.label(label);
+    //    rec.host(name());
+
+    //rec.port(22);
 
     var c = RemoteOper.SHARED_INSTANCE.cmdLineArgs();
     while (c.hasNextArg()) {
+      pr("next arg:", c.peekNextArg(), "rec:", INDENT, rec);
       if (c.nextArgIf("port")) {
         rec.port(Integer.parseInt(c.nextArg()));
         continue;
