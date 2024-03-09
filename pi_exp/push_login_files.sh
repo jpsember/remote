43a23a83e6a73cf5ac81bc530aea20976571bb24
Jeff Sember <jpsember@gmail.com>
2024-03-08 19:36:22 -0800

    Issue #2

--- pi_exp/push_login_files.sh
+++ pi_exp/push_login_files.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -eu
 
-HOME=/home/pi
+echo "Attempting to install some files on the rpi."
+echo "If prompted, enter the password 'password'."
+
+# The remote's project_dir is set to the remote's user directory, so we can
+# push relative to that.
+
 remote user select pi
+dev push authorized_keys .ssh/authorized_keys
 dev push bash_profile.txt .bash_profile
 dev push inputrc.txt .inputrc
 dev push hush_login.txt .hushlogin
